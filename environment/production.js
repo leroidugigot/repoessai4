e83216f89144fa8df1727a5926fbf9de252e64ee
Leroidugigot <familleaitbella@gmail.com>
2024-07-23 00:31:18 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -1,5 +1,5 @@
 module.exports = {
     dbUrl: 'mongodb+srv://familleaitbella:123@clustermomo.5krbhd5.mongodb.net/?retryWrites=true&w=majority&appName=Clustermomo',
-    cert: '',
-    key: ''
+    cert: '/etc/letsencrypt/live/www.momocoptere.site/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.momocoptere.site/privkey.pem'
   }
