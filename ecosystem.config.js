50f50f488dced68f6657328a372855620b1c7655
Leroidugigot <familleaitbella@gmail.com>
2024-07-24 16:06:15 +0200

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   apps : [{
+    name: 'twitter',
     script: './bin/www',
-    watch: 'NODE_ENV:development ',
-    autorestart : true,
-    instances : '8'
-  }, {
-    script: './bin/www',
-    watch: 'NODE_ENV:production',
-    autorestart : true,
-    instances : 'max'
+    instances: 'max',
+    autorestart: true,
+    watch: true,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }],
-
-
 };
