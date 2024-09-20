33c80de4f1fe6d45d3fe6bbbfa7e6f5370c7a526
momo <familleaitbella@gmail.com>
2024-09-20 20:34:06 +0200

    update changement ecosysteme.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  apps : [{
-    name: 'twitter',
-    script: './bin/www',
-    instances: 'max',
-    autorestart: true,
-    watch: true,
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: "momocoptere",
+      script: "./bin/www",
+      instances: "max",
+      autorestart: true,
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+  ],
 };
