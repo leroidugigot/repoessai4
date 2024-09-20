1566acf082bb4e9802a2881461b8c3ed60e31aa3
momo <familleaitbella@gmail.com>
2024-09-20 22:54:22 +0200

    path cert et key rectification

--- environment/production.js
+++ environment/production.js
@@ -1,8 +1,8 @@
 module.exports = {
   dbUrl:
     "mongodb+srv://familleaitbella:123@clustermomo.5krbhd5.mongodb.net/?retryWrites=true&w=majority&appName=Clustermomo",
-  cert: "../../archive/www.momocoptere.site/fullchain1.pem",
-  key: "../../archive/www.momocoptere.site/privkey1.pem",
+  cert: "/etc/letsencrypt/live/www.momocoptere.site/fullchain.pem",
+  key: "/etc/letsencrypt/live/www.momocoptere.site/privkey.pem",
   portHttp: 80,
   portHttps: 443,
 };
