8da1225f72872f21524c8949107b67c9bb94481f
leroidugigot <famileaitbella@gmail.com>
2024-10-14 18:46:14 +0200

    duree session et jwt ajusté à 1h

--- config/session.config.js
+++ config/session.config.js
@@ -3,6 +3,7 @@ const MongoStore = require('connect-mongo');
 const { clientPromise } = require('../database'); // clientPromise vient du fichier de connexion
 const mongoose = require('mongoose'); // Import de mongoose
 const { app } = require('../app');
+require('dotenv').config();
 
 // Middleware pour gérer les erreurs de connexion
 const handleMongoConnection = async () => {
@@ -25,30 +26,31 @@ const handleMongoConnection = async () => {
   }
 };
 
+const sessionSecret = process.env.SESSION_SECRET;
+
 
 app.use(
   session({
-    secret: 'cersei',
+    secret: sessionSecret,
     resave: false,
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 14, // session de 14 jours
+      maxAge: 1000 * 60 * 60, // Durée de vie du cookie de session : 1 heure avec deconnection
     },
     store: MongoStore.create({
-      clientPromise: handleMongoConnection(), // Utiliser la fonction pour gérer la connexion
+      clientPromise: handleMongoConnection(), // Fonction pour gérer la connexion à MongoDB
       dbName: 'test', // Assurez-vous de spécifier le nom de la base de données
-      ttl: 60 * 60 * 24 * 14, // session de 14 jours
+      ttl: 60 * 60 * 24 * 365, // Durée de vie de la session dans le store : 1 an avant effacement donnee de l'user de la bdd
     }),
   })
 );
 
 // Log pour vérifier que la session est correctement configurée
-app.use((req, res, next) => {
-  console.log(`Session configurée pour l'utilisateur: ${req.session.userId || 'Invité'}`);
+app.use((req, res, next) => { 
+  console.log('Contenu de la session:', req.session);
   next();
 });
-
 // Log d'erreur pour la session
 app.use((err, req, res, next) => {
   console.error('Erreur dans le middleware de session:', err);
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,38 +6,47 @@ exports.userNew = (req, res, next) => {
   res.render('signup', { error: null, siteKey });
 };
 
-exports.userCreate = async (req, res, next) => {
-  const captcha = req.body['g-recaptcha-response'];
 
-  // Vérifiez si le captcha est présent
-  if (!captcha) {
-      return res.status(400).json({ message: 'Please complete the CAPTCHA' });
-  }
-
-  try {
-    console.log('Secret Key:', process.env.RECAPTCHA_SECRET_KEY); // À supprimer en production
-    // Vérification avec l'API de Google reCAPTCHA
-    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
-        params: {
-            secret: process.env.RECAPTCHA_SECRET_KEY,
-            response: captcha,
-        },
-    });
-
-    const { success } = response.data;
-
-    if (success) {
-      // Si le captcha est validé, redirigez vers protected.ejs
-      res.redirect('/protected'); // ou utilisez res.render('protected') si vous voulez juste rendre la vue
-    } else {
-      // Si la validation du captcha échoue
-      res.status(400).send('Captcha verification failed. Please try again.');
-    }
-
-  } catch (error) {
-    console.error('Error verifying CAPTCHA:', error);
-    res.status(500).send('An error occurred during CAPTCHA verification.');
-  }
-};
-
-
+              //authtification via formulaire
+              exports.userCreate = async (req, res, next) => {
+                const captcha = req.body['g-recaptcha-response'];
+              
+                // Vérifiez si le captcha est présent
+                if (!captcha) {
+                    return res.status(400).json({ message: 'Please complete the CAPTCHA' });
+                }
+              
+                try {
+                  // Vérification avec l'API de Google reCAPTCHA
+                  const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
+                      params: {
+                          secret: process.env.RECAPTCHA_SECRET_KEY,
+                          response: captcha,
+                      },
+                  });
+              
+                  const { success } = response.data;
+              
+                  if (success) {
+                    // Si le captcha est validé, on crée l'utilisateur
+                    const body = req.body;
+                    const user = await createUser(body);
+                    
+                    // Passer une fonction de rappel à req.login
+                    req.login(user, (err) => {
+                      if (err) {
+                        return next(err); // Gérer l'erreur de connexion
+                      }
+                      res.redirect('/protected'); // Rediriger après connexion réussie
+                    });
+                  } else {
+                    // Si la validation du captcha échoue
+                    res.status(400).send('Captcha verification failed. Please try again.');
+                  }
+              
+                } catch (error) {
+                  console.error('Error verifying CAPTCHA:', error);
+                  res.status(500).send('An error occurred during CAPTCHA verification.');
+                }
+              };
+              
