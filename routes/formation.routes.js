061733aca8e2e7ae8beba5a5cfb3cfd23027e4c0
leroidugigot <famileaitbella@gmail.com>
2024-10-31 08:37:31 +0100

    fixed variables prtoteced

--- routes/formation.routes.js
+++ routes/formation.routes.js
@@ -24,8 +24,15 @@ router.get("/protected", ensureAuthenticated, async (req, res) => {
         const user = req.user; // Assurez-vous que l'utilisateur est bien attaché à la requête
         console.log("Utilisateur:", user);
         console.log("Formations:", formations);
-        
-        res.render("protected", { user, formations,module });
+         // Exemple de récupération d'un module spécifique (par exemple, premier module de la première formation)
+         const module = formations[0]?.modules[0] || null;
+         console.log(module);
+
+                 // Extraire les modules uniquement
+        const modules = formations.flatMap(formation => formation.modules); // fusionne les modules de toutes les formations en un seul tableau
+         console.log(modules);
+         
+        res.render("protected", { user, formations,module ,modules});
     } catch (error) {
         console.error("Erreur lors de la récupération des formations:", error);
         res.status(500).send("Erreur lors de la récupération des formations");
