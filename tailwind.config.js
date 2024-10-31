a22e89c6ce1851caaf93ef2339f6b408ec5996f2
leroidugigot <famileaitbella@gmail.com>
2024-11-01 00:09:15 +0100

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.html", "./src/**/*.js", "./public/**/*.html", "./public/**/*.js","./views/*.ejs","./views/**/*.ejs","./views/**/**/*.ejs,","./views/*.js","./views/**/*.js","./views/**/**/*.js" ],// Use './' for relative paths
+  content: ["./src/**/*.html", "./src/**/*.js", "./public/**/*.html", "./public/**/*.js","./views/*.ejs","./views/**/*.ejs","./views/**/**/*.ejs,","./views/**/**/**/*.ejs,","./views/**/**/**/**/*.ejs,","./views/**/**/**/**/**/*.ejs,","./views/*.js","./views/**/*.js","./views/**/**/*.js" ],// Use './' for relative paths
   theme: {
     extend: {},
   },
